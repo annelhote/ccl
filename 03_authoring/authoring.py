b580c6a24352bfc4af1d4c46c36b0bb22280afc2
annelhote <anne.lhote@gmail.com>
2014-12-11 10:29:28 +0100

    Merge emails.py into authoring.py

--- 03_authoring/authoring.py
+++ 03_authoring/authoring.py
@@ -3,9 +3,6 @@
 # python authoring.py
 
 
-# @TODO : Merge with emails.py
-
-
 # Imports
 from pymongo import MongoClient
 
@@ -21,10 +18,16 @@ if __name__ == "__main__":
 	# Clear all authors
 	authors.remove({})
 
+	# Iterate over mails
 	# for mail in mails.find({'url':{'$regex':'\?2001\+'}}):
 	for mail in mails.find():
 		email = mail['email']
+		# If email is not already an author, add it
 		if authors.find({'email':email}).count() == 0:
-			authors.insert({'email':email,'mails':[mail['_id']],'count':1})
+			authors.insert({'email':email,'mails':[mail['_id']],'count':1,'startdate':mail['timestamp'],'enddate':mail['timestamp']})
+		# Else increment the count and update the startdate and enddate fields if needed
 		else:
-			authors.update({'email':email},{'$push':{'mails':mail['_id']},'$inc':{'count':1}})
+			author = authors.find({'email':email})[0]
+			startdate = mail['timestamp'] if mail['timestamp'] < author['startdate'] else author['startdate']
+			enddate = mail['timestamp'] if mail['timestamp'] > author['enddate'] else author['enddate']
+			authors.update({'_id':author['_id']},{'$set':{'startdate':startdate,'enddate':enddate},'$push':{'mails':mail['_id']}, '$inc':{'count':1}})

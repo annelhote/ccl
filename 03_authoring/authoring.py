9b0baf19cc8e935fdc01f7fdcb191b3821a7911d
annelhote <anne.lhote@gmail.com>
2015-01-05 15:01:33 +0100

    Review the email authoring

--- 03_authoring/authoring.py
+++ 03_authoring/authoring.py
@@ -4,9 +4,8 @@
 
 
 # Imports
-import io, datetime, re
+import datetime, io, pymongo, re
 from pprint import pprint
-from pymongo import MongoClient
 
 
 # Log the message level and the message into a log file
@@ -20,35 +19,35 @@ def log(file, level, message):
 
 
 # Log the message level and the message into a log file
-# email01			: string		: email of the first author to merge
-# email02			: string		: email of the second author to merge
+# author01			: object		: first author to merge
+# author02			: object		: second author to merge
 # return			: void
-def mergeAuthors(email01, email02):
-	log(f, 'info', 'Merge email addresses : ' + email01 + ' and ' + email02)
-	if authorsCollection.find({'emails':email01,'deleted':{'$exists':False}}).count() == 1 and authorsCollection.find({'emails':email02,'deleted':{'$exists':False}}).count() == 1:
-		author01 = authorsCollection.find({'emails':email,'deleted':{'$exists':False}})[0]
-		author02 = authorsCollection.find({'emails':email,'deleted':{'$exists':False}})[0]
-		emails = author01['emails'] + author02['emails']
-		mails = author01['mails'] + author02['mails']
-		count = author01['count'] + author02['count']
-		startdate = author01['startdate'] if author01['startdate'] < author02['startdate'] else author02['startdate']
-		enddate = author01['enddate'] if author01['enddate'] > author02['enddate'] else author02['enddate']
-		# Insert the merged author into collection
-		try:
-			authorsCollection.insert({'emails':emails,'mails':mails,'count':count,'startdate':startdate,'enddate':enddate,'merged':1})
-		except:
-			log(f, 'error', 'Size of the list of emails to insert : ' + str(len(mails)))
-		# Mark both authors as deleted
-		authorsCollection.update({'_id':author01['_id']},{'$set':{'deleted':1}})
-		authorsCollection.update({'_id':author01['_id']},{'$set':{'deleted':1}})
-	else:
-		log(f, 'warning', 'Duplicate email address into authors collection (either ' + email01 + ' or ' + email02 + ')')
+def mergeAuthors(author01, author02):
+	log(f, 'info', 'Merge authors : ' + str(author01['_id']) + ' and ' + str(author02['_id']))
+	# Merge all the email addresses together and remove duplicates
+	emails		= list(set(author01['emails'] + author02['emails']))
+	# Merge all the mails together and remove duplicates
+	mails 		= list(set(author01['mails'] + author02['mails']))
+	count 		= len(mails)
+	startdate 	= author01['startdate'] if author01['startdate'] < author02['startdate'] else author02['startdate']
+	enddate 	= author01['enddate'] if author01['enddate'] > author02['enddate'] else author02['enddate']
+	duration 	= enddate - startdate
+	duration	= 1 if duration == 0 else duration
+	# Insert the merged author into collection
+	try:
+		authorsCollection.insert({'emails':emails,'mails':mails,'count':count,'startdate':startdate,'enddate':enddate,'duration':duration,'merged':1})
+	except:
+		log(f, 'error', 'Try to merge ' + email01 + ' and ' + email02)
+		log(f, 'error', 'Size of the list of emails to insert : ' + str(len(mails)))
+	# Mark both authors as deleted
+	authorsCollection.update({'_id':author01['_id']},{'$set':{'deleted':1}})
+	authorsCollection.update({'_id':author01['_id']},{'$set':{'deleted':1}})
 
 
 # Main
 if __name__ == "__main__":
 	# MongoDB connection
-	client 				= MongoClient('localhost', 27017)
+	client 				= pymongo.MongoClient('localhost', 27017)
 	db 					= client.ccl
 	mailsCollection 	= db.mails
 	authorsCollection 	= db.authors
@@ -57,38 +56,52 @@ if __name__ == "__main__":
 	authorsCollection.remove({})
 
 	# Open a log file
-	f = io.open('error.log', 'w', encoding='utf-8')
+	f = io.open('authoring.log', 'w', encoding='utf-8')
 	log(f, 'info', 'Start authoring')
 
-	# Iterate over mails
-	# for mail in mails.find({'url':{'$regex':'\?2001\+'}}):
-	for mail in mailsCollection.find():
+	# Iterate over mails that have no @ in their email address
+	for mail in mailsCollection.find({'email':{'$regex':'^[^@]*$'},'deleted':{'$exists':0}}).sort('emaillength',pymongo.DESCENDING):
 		email = mail['email']
-		# If email is not already an author, add it
-		if authorsCollection.find({'emails':email,'deleted':{'$exists':False}}).count() == 0:
-			authorsCollection.insert({'emails':[email],'mails':[mail['_id']],'count':1,'startdate':mail['timestamp'],'enddate':mail['timestamp']})
-		# Else increment the count and update the startdate and enddate fields if needed
-		else:
-			author = authorsCollection.find({'emails':email,'deleted':{'$exists':False}})[0]
-			startdate = mail['timestamp'] if mail['timestamp'] < author['startdate'] else author['startdate']
-			enddate = mail['timestamp'] if mail['timestamp'] > author['enddate'] else author['enddate']
-			authorsCollection.update({'_id':author['_id']},{'$set':{'startdate':startdate,'enddate':enddate},'$push':{'mails':mail['_id']}, '$inc':{'count':1}})
-
-	for author in authorsCollection.find():
-		for email in author['emails']:
-			# Get first part of the author email before the @
-			if re.search(r'(.*)@', email):
-				trunk = re.search(r'(.*)@', email).group(1)
-				# Find similar email addresses
-				similaremails = authorsCollection.find({'emails':{'$regex':'^' + trunk + '@'}})
-				for similaremail in similaremails:
-					if len(similaremail['emails']) > 1:
-						log(f,'error','The size of this author\'s emails is bigger than 1 : ' + str(author['_id']))
-					else:
-						# Merge them together
-						mergeAuthors(email,similaremail['emails'][0])
+		if email != '':
+			# If that email already exists
+			if authorsCollection.find({'emails':{'$regex':email},'deleted':{'$exists':0}}).count() > 0:
+				author 		= authorsCollection.find({'emails':{'$regex':email},'deleted':{'$exists':0}})[0]
+				emails		= author['emails'] if email in author['emails'] else author['emails'] + [email]
+				startdate	= mail['timestamp'] if mail['timestamp'] < author['startdate'] else author['startdate']
+				enddate		= mail['timestamp'] if mail['timestamp'] > author['enddate'] else author['enddate']
+				duration	= enddate - startdate
+				duration	= 1 if duration == 0 else duration
+				authorsCollection.update({'_id':author['_id']},{'$set':{'emails':emails,'startdate':startdate,'enddate':enddate,'duration':duration},'$push':{'mails':mail['_id']}, '$inc':{'count':1}})
+			# Else add it
 			else:
-				log(f,'info','This email address has no @ : ' + email)
+				authorsCollection.insert({'emails':[email],'mails':[mail['_id']],'count':1,'startdate':mail['timestamp'],'enddate':mail['timestamp'],'duration':1})
+
+	# Iterate over mails that have an @ in their email address
+	for mail in mailsCollection.find({'email':{'$regex':'@'},'deleted':{'$exists':0}}):
+		email = mail['email']
+		trunk = re.search(r'(.*)@', email).group(1)
+		regex = re.compile('^' + trunk + '@')
+		# If that email already exists
+		if email != '' and authorsCollection.find({'emails':regex,'deleted':{'$exists':0}}).count() > 0:
+			authors			= authorsCollection.find({'emails':regex,'deleted':{'$exists':0}}).sort('emaillength',pymongo.DESCENDING)
+			emails 			= [email]
+			mails 			= [mail['_id']]
+			startdate 		= mail['timestamp']
+			enddate 		= mail['timestamp']
+			for author in authors:
+				emails		= list(set(emails + author['emails']))
+				mails 		= list(set(author['mails'] + [mail['_id']]))
+				startdate	= min(startdate, author['startdate'])
+				enddate		= max(enddate, author['enddate'])
+				# Mark as deleted
+				authorsCollection.update({'_id':author['_id']},{'$set':{'deleted':1}})
+			count			= len(emails)
+			duration		= enddate - startdate
+			duration		= 1 if duration == 0 else duration
+			authorsCollection.insert({'emails':emails,'count':count,'mails':mails,'startdate':startdate,'enddate':enddate,'duration':duration})
+		# Else add it
+		else:
+			authorsCollection.insert({'emails':[email],'mails':[mail['_id']],'count':1,'startdate':mail['timestamp'],'enddate':mail['timestamp'],'duration':1})
 
 	# Close the log file
 	log(f, 'info', 'End authoring')
